90f87eb544ecc0c49e7da0baff32f53a0d60e4c8
Relative <relative@outlook.ie>
2018-01-19 16:05:57 -0500

    Device properties and tests for them

--- src/HomeDevice.js
+++ src/HomeDevice.js
@@ -1,6 +1,23 @@
 const axios = require('axios')
 const moment = require('moment')
 
+/**
+ * @typedef {Object} DeviceVersionInfo
+ * @prop {String} systemBuildNumber - Build number
+ * @prop {String} releaseTrack - Track of release
+ * @prop {String} castBuildRev - Cast build revision
+ * @prop {Number} previewChannelState - Preview channel state
+ * @prop {Boolean} preivewChannel - In preview channel
+ */
+
+/**
+ * @typedef {Object} DeviceIdentifiers
+ * @prop {String} hotspotMAC - MAC address of the Wi-Fi setup network
+ * @prop {String} macAddress - MAC address of the Wi-Fi card in the Home
+ * @prop {String} cloudDeviceId - Cloud device id?
+ * @prop {String} umaClientId - UMA Client Id?
+ */
+
 class HomeDevice {
   /**
    * Home device
@@ -43,6 +60,30 @@ class HomeDevice {
      */
     this.lastPingData = {}
 
+    /**
+     * Device name
+     * @type {String}
+     */
+    this.name = null
+
+    /**
+     * Device version info
+     * @type {DeviceVersionInfo}
+     */
+    this.version = {}
+
+    /**
+     * Device time zone
+     * @type {String}
+     */
+    this.timezone = null
+
+    /**
+     * Device identifiers (unique)
+     * @type {DeviceIdentifiers}
+     */
+    this.identifiers = {}
+
     this.ping()
   }
 
@@ -50,16 +91,43 @@ class HomeDevice {
    * Device online check handler
    */
   async ping() {
-    const resp = await this.request('/setup/eureka_info?options=version,name,build_info,device_info,net,wifi,setup,settings,opt_in,opencast,multizone,proxy,night_mode_params,user_eq,room_equalizer')
+    const resp = await this.request('/setup/eureka_info?params=version,name,build_info,device_info,net,wifi,setup,settings,opt_in,opencast,multizone,proxy,night_mode_params,user_eq,room_equalizer&options=detail')
     if (typeof resp !== 'object') {
       this.lastPing = moment()
       this.lastPingData = resp
       this.connected = false
-      return this.disconnect()
+      this.disconnect()
     } else {
-      this.connected = resp.connected
+      this.connected = true
       this.lastPing = moment()
       this.lastPingData = resp
+      this.updateMeta(resp)
+    }
+    return this.connected
+  }
+
+  /**
+   * Update the metadata on the class
+   * @param {Object} data - Update data
+   */
+  updateMeta(data) {
+    this.name = data.name
+
+    this.version = {
+      systemBuildNumber: data.build_info.system_build_number,
+      releaseTrack: data.build_info.release_track,
+      castBuildRev: data.build_info.cast_build_revision,
+      previewChannelState: data.build_info.preview_channel_state,
+      previewChannel: data.opt_in.preview_channel
+    }
+
+    this.timezone = data.settings.timezone
+
+    this.identifiers = {
+      hotspotMAC: data.device_info.hotspot_bssid,
+      macAddress: data.device_info.mac_address,
+      cloudDeviceId: data.device_info.cloud_device_id,
+      umaClientId: data.device_info.uma_client_id
     }
   }
 
--- test/test.js
+++ test/test.js
@@ -11,5 +11,24 @@ describe('HomeDevice', () => {
       resp.status.should.equal(200)
       resp.error.should.equal(false)
     })
+    it('should have identifiers, version info and a name on the class', async () => {
+      await device.ping()
+      device.connected.should.equal(true)
+
+      device.name.should.be.a('string')
+      
+      device.version.should.be.a('object')
+      device.version.castBuildRev.should.be.a('string')
+      device.version.previewChannel.should.be.a('boolean')
+      device.version.previewChannelState.should.be.a('number')
+      device.version.releaseTrack.should.be.a('string')
+      device.version.systemBuildNumber.should.be.a('string')
+
+      device.identifiers.should.be.a('object')
+      device.identifiers.cloudDeviceId.should.be.a('string')
+      device.identifiers.hotspotMAC.should.be.a('string')
+      device.identifiers.macAddress.should.be.a('string')
+      device.identifiers.umaClientId.should.be.a('string')
+    })
   })
 })
